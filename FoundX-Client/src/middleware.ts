2beba60fd4c60ef895c1c794ed795928fadc5fde
Nabiur siddique <nabiursiddique01@gmail.com>
2024-09-13 21:35:12 +0600

    middleware file is added to implement authentication

---
+++ FoundX-Client/src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const user = {
+    name: "Nabil",
+    token: "adasfs",
+    role: "USER",
+  };
+
+  if (user?.name) {
+    return NextResponse.next();
+  } else {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.redirect(new URL("/", request.url));
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/profile", "/admin"],
+};
