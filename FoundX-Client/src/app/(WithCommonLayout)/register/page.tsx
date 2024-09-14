90be6d1342397aa49ba75d4b7231435438927ed6
Nabiur siddique <nabiursiddique01@gmail.com>
2024-09-14 23:51:24 +0600

    Loading, success and errors are handled with toast

--- FoundX-Client/src/app/(WithCommonLayout)/register/page.tsx
+++ FoundX-Client/src/app/(WithCommonLayout)/register/page.tsx
@@ -2,28 +2,15 @@
 
 import FXForm from "@/src/components/form/FXForm";
 import FXInput from "@/src/components/form/FXInput";
+import { useUserRegistration } from "@/src/hooks/auth.hook";
 import registerValidationSchema from "@/src/schemas/register.schema";
-import { registerUser } from "@/src/services/AuthService";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@nextui-org/button";
-import { useMutation } from "@tanstack/react-query";
 import Link from "next/link";
 import { FieldValues, SubmitHandler } from "react-hook-form";
 
 const RegisterPage = () => {
-  const {
-    mutate: handleUserRegistration,
-    isPending,
-    data,
-    isError,
-    isSuccess,
-  } = useMutation({
-    mutationKey: ["USER_REGISTRATION"],
-    mutationFn: async (userData) => await registerUser(userData),
-    onSuccess: () => {
-      console.log("User creation successful");
-    },
-  });
+  const { mutate: handleUserRegistration, isPending } = useUserRegistration();
 
   const onSubmit: SubmitHandler<FieldValues> = async (data) => {
     const userData = {
@@ -36,6 +23,11 @@ const RegisterPage = () => {
 
     handleUserRegistration(userData);
   };
+
+  if (isPending) {
+    // handle loading state
+    return <p>Loading...</p>;
+  }
 
   return (
     <div className="flex h-[calc(100vh-100px)] flex-col items-center justify-center">
