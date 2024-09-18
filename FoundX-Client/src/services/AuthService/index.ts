2424edb27ec3c794279c8354cce61abe611dfbd2
Nabiur siddique <nabiursiddique01@gmail.com>
2024-09-18 15:25:41 +0600

    user provider is created

--- FoundX-Client/src/services/AuthService/index.ts
+++ FoundX-Client/src/services/AuthService/index.ts
@@ -58,6 +58,7 @@ export const getCurrentUser = async () => {
       mobileNumber: decodedToken.mobileNumber,
       role: decodedToken.role,
       status: decodedToken.status,
+      profilePhoto: decodedToken.profilePhoto,
     };
   }
 
