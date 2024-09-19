25b226896bb376ad61aa90944c95e8b5da994044
Nabiur siddique <nabiursiddique01@gmail.com>
2024-09-20 02:11:15 +0600

    cookies errors and cache revalidation is handled

--- FoundX-Client/src/app/(WithCommonLayout)/(user)/profile/create-post/page.tsx
+++ FoundX-Client/src/app/(WithCommonLayout)/(user)/profile/create-post/page.tsx
@@ -19,6 +19,9 @@ import { ChangeEvent, useState } from "react";
 import FXTextarea from "@/src/components/form/FXTextarea";
 import { AddIcon, TrashIcon } from "@/src/assets/icons";
 import { useUser } from "@/src/context/user.provider";
+import { useCreatePost } from "@/src/hooks/post.hook";
+import Loading from "@/src/components/UI/Loading";
+import { useRouter } from "next/navigation";
 
 const cityOptions = allDistict()
   .sort()
@@ -33,8 +36,14 @@ const CreatePost = () => {
   const [imageFiles, setImageFiles] = useState<File[] | []>([]);
   const [imagePreviews, setImagePreviews] = useState<string[] | []>([]);
 
-  const { user } = useUser();
+  const router = useRouter();
 
+  const {
+    mutate: handleCreatePost,
+    isPending: createPostPending,
+    isSuccess,
+  } = useCreatePost();
+  const { user } = useUser();
   const {
     data: categoriesData,
     isLoading: categoryLoading,
@@ -53,9 +62,7 @@ const CreatePost = () => {
   }
 
   const methods = useForm();
-
   const { control, handleSubmit } = methods;
-
   const { fields, append, remove } = useFieldArray({
     control,
     name: "questions",
@@ -77,8 +84,7 @@ const CreatePost = () => {
       formData.append("itemImages", image);
     }
 
-    console.log(formData.get("data"));
-    console.log(formData.get("itemImages"));
+    handleCreatePost(formData);
   };
 
   const handleFieldAppend = () => {
@@ -99,110 +105,117 @@ const CreatePost = () => {
     }
   };
 
-  return (
-    <div className="h-full rounded-xl bg-gradient-to-b from-default-100 px-[73px] py-12">
-      <h1 className="text-2xl font-semibold">Post a found item</h1>
-      <Divider className="mb-5 mt-3" />
-      <FormProvider {...methods}>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="flex flex-wrap gap-2 py-2">
-            <div className="min-w-fit flex-1">
-              <FXInput label="Title" name="title" />
-            </div>
-            <div className="min-w-fit flex-1">
-              <FXDatePicker label="Found date" name="dateFound" />
-            </div>
-          </div>
-          <div className="flex flex-wrap gap-2 py-2">
-            <div className="min-w-fit flex-1">
-              <FXInput label="Location" name="location" />
-            </div>
-            <div className="min-w-fit flex-1">
-              <FXSelect label="City" name="city" options={cityOptions} />
-            </div>
-          </div>
-          <div className="flex flex-wrap gap-2 py-2">
-            <div className="min-w-fit flex-1">
-              <FXSelect
-                disabled={!categorySuccess}
-                label="Category"
-                name="category"
-                options={categoryOption}
-              />
-            </div>
-            <div className="min-w-fit flex-1">
-              <label
-                className="flex h-14 w-full cursor-pointer items-center justify-center rounded-xl border-2 border-default-200 text-default-500 shadow-sm transition-all duration-100 hover:border-default-400"
-                htmlFor="image"
-              >
-                Upload image
-              </label>
-              <input
-                multiple
-                className="hidden"
-                id="image"
-                type="file"
-                onChange={(e) => handleImageChange(e)}
-              />
-            </div>
-          </div>
+  if (!createPostPending && isSuccess) {
+    router.push("/");
+  }
 
-          {imagePreviews.length > 0 && (
-            <div className="flex gap-5 my-5 flex-wrap">
-              {imagePreviews.map((imageDataUrl) => (
-                <div
-                  key={imageDataUrl}
-                  className="relative size-48 rounded-xl border-2 border-dashed border-default-300 p-2"
+  return (
+    <>
+      {createPostPending && <Loading />}
+      <div className="h-full rounded-xl bg-gradient-to-b from-default-100 px-[73px] py-12">
+        <h1 className="text-2xl font-semibold">Post a found item</h1>
+        <Divider className="mb-5 mt-3" />
+        <FormProvider {...methods}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-wrap gap-2 py-2">
+              <div className="min-w-fit flex-1">
+                <FXInput label="Title" name="title" />
+              </div>
+              <div className="min-w-fit flex-1">
+                <FXDatePicker label="Found date" name="dateFound" />
+              </div>
+            </div>
+            <div className="flex flex-wrap gap-2 py-2">
+              <div className="min-w-fit flex-1">
+                <FXInput label="Location" name="location" />
+              </div>
+              <div className="min-w-fit flex-1">
+                <FXSelect label="City" name="city" options={cityOptions} />
+              </div>
+            </div>
+            <div className="flex flex-wrap gap-2 py-2">
+              <div className="min-w-fit flex-1">
+                <FXSelect
+                  disabled={!categorySuccess}
+                  label="Category"
+                  name="category"
+                  options={categoryOption}
+                />
+              </div>
+              <div className="min-w-fit flex-1">
+                <label
+                  className="flex h-14 w-full cursor-pointer items-center justify-center rounded-xl border-2 border-default-200 text-default-500 shadow-sm transition-all duration-100 hover:border-default-400"
+                  htmlFor="image"
                 >
-                  <img
-                    alt="item"
-                    className="h-full w-full object-cover object-center rounded-md"
-                    src={imageDataUrl}
-                  />
+                  Upload image
+                </label>
+                <input
+                  multiple
+                  className="hidden"
+                  id="image"
+                  type="file"
+                  onChange={(e) => handleImageChange(e)}
+                />
+              </div>
+            </div>
+
+            {imagePreviews.length > 0 && (
+              <div className="flex gap-5 my-5 flex-wrap">
+                {imagePreviews.map((imageDataUrl) => (
+                  <div
+                    key={imageDataUrl}
+                    className="relative size-48 rounded-xl border-2 border-dashed border-default-300 p-2"
+                  >
+                    <img
+                      alt="item"
+                      className="h-full w-full object-cover object-center rounded-md"
+                      src={imageDataUrl}
+                    />
+                  </div>
+                ))}
+              </div>
+            )}
+
+            <div className="flex flex-wrap-reverse gap-2 py-2">
+              <div className="min-w-fit flex-1">
+                <FXTextarea label="Description" name="description" />
+              </div>
+            </div>
+
+            <Divider className="my-5" />
+
+            <div className="flex justify-between items-center mb-5">
+              <h1 className="text-xl">Owner verification questions</h1>
+              <Button isIconOnly onClick={() => handleFieldAppend()}>
+                <AddIcon />
+              </Button>
+            </div>
+
+            <div className="space-y-5">
+              {fields.map((field, index) => (
+                <div key={field.id} className="flex gap-2 items-center">
+                  <FXInput label="Question" name={`questions.${index}.value`} />
+                  <Button
+                    isIconOnly
+                    className="h-14 w-16"
+                    onClick={() => remove(index)}
+                  >
+                    <TrashIcon />
+                  </Button>
                 </div>
               ))}
             </div>
-          )}
 
-          <div className="flex flex-wrap-reverse gap-2 py-2">
-            <div className="min-w-fit flex-1">
-              <FXTextarea label="Description" name="description" />
+            <Divider className="my-5" />
+            <div className="flex justify-end">
+              <Button size="lg" type="submit">
+                Post
+              </Button>
             </div>
-          </div>
-
-          <Divider className="my-5" />
-
-          <div className="flex justify-between items-center mb-5">
-            <h1 className="text-xl">Owner verification questions</h1>
-            <Button isIconOnly onClick={() => handleFieldAppend()}>
-              <AddIcon />
-            </Button>
-          </div>
-
-          <div className="space-y-5">
-            {fields.map((field, index) => (
-              <div key={field.id} className="flex gap-2 items-center">
-                <FXInput label="Question" name={`questions.${index}.value`} />
-                <Button
-                  isIconOnly
-                  className="h-14 w-16"
-                  onClick={() => remove(index)}
-                >
-                  <TrashIcon />
-                </Button>
-              </div>
-            ))}
-          </div>
-
-          <Divider className="my-5" />
-          <div className="flex justify-end">
-            <Button size="lg" type="submit">
-              Post
-            </Button>
-          </div>
-        </form>
-      </FormProvider>
-    </div>
+          </form>
+        </FormProvider>
+      </div>
+    </>
   );
 };
 
